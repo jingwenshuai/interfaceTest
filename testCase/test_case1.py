a53b4d19b4ae18df5a4d2644ef010300628717e1
jingwenshuai <18911222403>
2019-05-18 19:09:06 +0800

    接口框架代码

---
+++ testCase/test_case1.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+#__author__= 'jws'
+
+import requests
+
+#调用readExcel模块拿到测试数据
+from common import readExcel
+
+#根据接口的请求方式判断调用什么方法(get/post)
+
+#get/post请求
+
+#校验
+
+#保存执行结果
+
+#写入excel中
