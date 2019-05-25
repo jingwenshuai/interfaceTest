8b4167b896945556ee606acb911fa4dc05ed3d56
jingwenshuai <18911222403>
2019-05-25 12:01:29 +0800

    接口框架代码

--- common/readExcel.py
+++ common/readExcel.py
@@ -52,16 +52,51 @@ class readExcel:
                     self.sheetData1.append(sheet_row)
                 elif self.sheetall.index(i) == 2:
                     self.sheetData2.append(sheet_row)
-        print(self.sheetData)
+        print('sheetData:',self.sheetData)
         print(type(self.sheetData))
         print(self.sheetData1)
-        print(self.sheetData2)
+        print('sheetData1是个DATA1:',self.sheetData1)
+        print('DATA2:',self.sheetData2)
 
     def addDate(self):
-        data = self.sheetData[0] + self.sheetData1[0][1:]
-        print('#######',data)
+        dat = []
+        sheet3 = []
+        #将sheet3中的第二列存入列表
+        for a in self.sheetData2:
+            sheet3.append(a[1])
+        print('sheet3[1]:',sheet3)
+
+        for i in range(3):
+            #将sheet1列表中的元素（也是个列表）和sheet2的中的元素（也是个列表）相加
+            data = self.sheetData[i] + self.sheetData1[i][1:]
+            #将sheet3的数据追加到列表后
+            data.append(sheet3[i])
+
+            #追加到大列表中
+            dat.append(data)
+        print('#######',dat)
+
         pass
 
 read = readExcel()
 read.getData()
 read.addDate()
+
+
+# #列表方法zip
+# a = [1,2,3]
+# b = ['a','b','c']
+# c = [4,5,6]
+#
+# x = list(zip(a,b,c))
+# print(x)
+# # m = []
+# # for a,b,c in x:
+# #     print(a,c)
+# #     m.append((a,c))
+# #     print(m)
+# a = []
+# for i in x:
+#     print('i----',i)
+#     a.append((i[0],i[-1]))
+#     print('---',a)
--- testCase/test_case1.py
+++ testCase/test_case1.py
@@ -1,12 +1,21 @@
 #coding=utf-8
 #__author__= 'jws'
 
+import re
 import requests
 
 #调用readExcel模块拿到测试数据
-from common import readExcel
+# from common import readExcel
 
 #根据接口的请求方式判断调用什么方法(get/post)
+# urlstr = 'https://wanandroid.com/user/login'
+# param = {'username':'jingwenshuai','password':'123456'}
+# r = requests.post(url=urlstr,data=param)
+# print(r.status_code)
+# a = r.text
+# print(a)
+# res = re.search('^jingwenshuai',a)
+# print(res)
 
 #get/post请求
 
