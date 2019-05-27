6dd23b738bcbaa2f14d4480353502d67468627d3
jingwenshuai <18911222403>
2019-05-27 22:28:45 +0800

    接口框架代码

---
+++ common/read_Http_test.py
@@ -0,0 +1,48 @@
+#coding=utf-8
+#__author__= 'jws'
+
+import requests
+
+class ReadHttp:
+
+    #定义get方法
+    def get(self,url,param):
+        try:
+            print(url,param)
+            response = requests.get(url=url,params=param)
+            result = response.text
+            return result
+        except Exception as msg:
+            print('please check out!-->',msg)
+            return None
+
+    #定义post方法
+    def post(self,url,param):
+        try:
+            print(url,param)
+            response = requests.post(url=url,data=param)
+            result = response.text
+            return result
+        except Exception as msg:
+            print('please check out!-->',msg)
+            return None
+
+    #定义put方法
+    def put(self,url,param):
+        try:
+            print(url,param)
+            response = requests.put(url=url)
+            result = response.text
+            return result
+        except Exception as msg:
+            print('please check out!-->',msg)
+            return None
+
+    #定义通过传参判断需要调用的方法
+    def get_Request(self,url,param,method):
+        if str(method) == 'get':
+            return self.get(url,param)
+        elif str(method) == 'post':
+            return self.post(url,param)
+        elif srt(method) == 'put':
+            return self.put(url,param)
---
+++ common/read_excel_test.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+#__author__= 'jws'
+
+
+import xlrd
+
+class ReadExcel:
+    #将打开excel方法赋与变量readbook
+    readbook = xlrd.open_workbook(r'C:\D\interfaceTest\testData\data.xls')
+    #通过实例化的readbook获取所有sheet页的名称
+    sheetall = readbook.sheet_names()
+    #创建存放sheet页数据的列表
+    urlSheet = []
+    paraamSheet = []
+    assertSheet =[]
+
+    #创建一个读取sheet页数据的方法
+    def getData(self):
+        for i in self.sheetall:
+            print(i)
+            #
+            sheet = self.readbook.sheet_by_name(i)
+            #获取sheet页最大行数
+            sheet_nrow = sheet.nrows
+            # print(sheet_nrow)
+            for j in range(1,sheet_nrow):
+                #获取行数据
+                sheet_row = sheet.row_values()
+                if self.sheetall.index(i) == 'urlSheet':
+                    self.urlSheet.append(sheet_row)
+                elif self.sheetall.index(i) =='paramSheet':
+                    self.paraamSheet.append(sheet_row)
+                elif self.sheetall.index(i) == 'assertSheet':
+                    self.assertSheet.append(sheet_row)
+
+    #创建一个重新构建数据的方法
+    def addData(self):
+        enData = []
+        for i in range(len(self.sheetall)):
+            #将列表中数据读出后按需要拼接
+            data = self.urlSheet[i] + self.paraamSheet[i][1:] + list(self.assertSheet[i][1])
+            #追加到列表中
+            enData.append(data)
+
+        return enData
+
+read = ReadExcel()
+read.getData()
+read.addData()
